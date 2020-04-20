0daa41c92dcf147222ccaa01d8b8eb9af98be3eb
Guilhem <gpanneau@MacBook-Pro-de-Guilhem.local>
2020-04-21 00:04:06 +0200

    modif setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="seals-gpanneau",
     version="0.0.1",
-    author="Guilhem PANNEAU, Dimitri MIKEC, Jonathan LOUISON",
+    author="gpanneau",
     author_email="guilhem.panneau@insa-lyon.fr",
     description="This package allows to model the impact of the sand extraction on 3 species of the Bay of the Somme - seals, soles and lugworms.",
     long_description= "This project allows to return four graph : * graph 1 : The evolution of the number of seals during the duration of the experiment. * graph 2 : The evolution of the number of soles during the duration of the experiment. * graph 3 : The evolution of the number of lugworms during the duration of the experiment. * graph 4 : The evolution of the amount of sand in cubic meters during the duration of the experiment.",
