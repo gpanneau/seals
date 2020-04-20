a71c1a59746bccdf934ca1f8673cfead4af8ed6c
MIKEC <dimitri.mikec@insa-lyon.fr>
2020-04-20 23:18:49 +0200

    modif setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author="Guilhem PANNEAU, Dimitri MIKEC, Jonathan LOUISON",
     author_email="guilhem.panneau@insa-lyon.fr",
     description="This package allows to model the impact of the sand extraction on 3 species of the Bay of the Somme - seals, soles and lugworms.",
-    long_description= "This project allows to return four graph : * graph 1 : The evolution of the number of seals during the duration of the experiment. * graph 2 : The evolution of the number of soles during the duration of the experiment. * graph 3 : The evolution of the number of lugworms during the duration of the experiment. * graph 4 : The evolution of the amount of sand in cubic meters during the duration of the experiment."
+    long_description= "This project allows to return four graph : * graph 1 : The evolution of the number of seals during the duration of the experiment. * graph 2 : The evolution of the number of soles during the duration of the experiment. * graph 3 : The evolution of the number of lugworms during the duration of the experiment. * graph 4 : The evolution of the amount of sand in cubic meters during the duration of the experiment.",
     long_description_content_type="text/markdown",
     url="https://github.com/gpanneau/seals",
     packages=setuptools.find_packages(),
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: CeCILL ",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.0',
 )
